6ca22eb429e899ea89df3daae59c1ce2ba263647
TJ Webb <webb.tj@gmail.com>
2016-03-17 20:23:12 -0300

    fix comment and sample data

--- src/includes/posts-to-posts.php
+++ src/includes/posts-to-posts.php
@@ -3,7 +3,7 @@
 add_action('p2p_init', 'taylor_register_relationships');
 function taylor_register_relationships(){
 
-//This next comment line is required for Taylor to append the post types correctly
+//This next comment line is required for Taylor to append the post type relationships correctly
 //P2P Connections
 
 }
